80292e0e7508c79df3b797c458f9cb84ab1443c6
alumasinde <143938688+alumasinde@users.noreply.github.com>
2026-08-16 09:12:13 +0300

    Harden frontend session initialization

--- frontend/app/bootstrap.php
+++ frontend/app/bootstrap.php
@@ -7,9 +7,11 @@ date_default_timezone_set($config['app']['timezone']);
 
 ini_set('session.use_strict_mode', '1');
 ini_set('session.use_only_cookies', '1');
+ini_set('session.use_trans_sid', '0');
 ini_set('session.cookie_httponly', '1');
 ini_set('session.cookie_secure', $config['security']['session_secure'] ? '1' : '0');
 ini_set('session.cookie_samesite', $config['security']['session_samesite']);
+ini_set('session.cookie_lifetime', '0');
 
 session_name($config['app']['session_name']);
 session_start();
