dee86b117275f2103982e39e8206026cc8b2c271
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-02 14:48:31 +0300

    feat(theme): wire theme handler into tenant APIs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -134,6 +134,7 @@ func buildApplication(db *sql.DB, cfg *config.Config) (*tenants.Repository, *rou
 	api.VisitorHandler = visitors.NewHandler(visitorSvc, idTypeRepo, companyRepo)
 	api.VisitorSettingsHandler = settings.NewVisitorSettingsHandler(settingsRepo)
 	api.GatepassSettingsHandler = settings.NewGatepassSettingsHandler(settingsRepo)
+	api.ThemeHandler = settings.NewThemeHandler(settingsRepo)
 	api.VisitTypeHandler = visits.NewVisitTypeHandler(visitTypeRepo)
 	api.DepartmentHandler = departments.NewHandler(deptRepo)
 	api.VisitHandler = visits.NewHandler(visitSvc)
